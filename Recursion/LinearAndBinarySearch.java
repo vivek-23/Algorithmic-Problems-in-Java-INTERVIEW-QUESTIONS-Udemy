1722e979a4c794e277963570cc80cfed3f47e8fa
Vivek Tadpatri <vivekpisces23@gmail.com>
2018-12-19 18:06:06 +0530

    LinearAndBinarySearch.java

--- Recursion/LinearAndBinarySearch.java
+++ Recursion/LinearAndBinarySearch.java
@@ -1,6 +1,6 @@
 import java.util.*;
 import java.io.*;
-public class Solution {
+public class LinearAndBinarySearch {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] s = br.readLine().split("\\s");
