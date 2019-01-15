a4dd3ac2c91f0804f4e9aa914166bc9093533792
Vivek Tadpatri <vivekpisces23@gmail.com>
2019-01-15 18:41:02 +0530

    FibonacciIterative.java

---
+++ Dynamic Programming/FibonacciIterative.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+public class FibonacciIterative {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(iterativeFibonacci(N));
+    }
+
+    private static int iterativeFibonacci(int n){
+        if(n <= 1) return n;
+        int[] dp = new int[n + 1];
+        dp[0] = 0;dp[1] = 1;
+        for(int i=2;i<dp.length;++i){
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
