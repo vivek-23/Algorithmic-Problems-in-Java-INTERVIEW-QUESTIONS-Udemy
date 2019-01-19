ea44276efd36cac9846cfff37ab99f64b1ca3329
Vivek Tadpatri <vivekpisces23@gmail.com>
2019-01-19 17:59:07 +0530

    IntegerReverse.java

--- Common Interview Questions/IntegerReverse.java
+++ Common Interview Questions/IntegerReverse.java
@@ -1,6 +1,6 @@
 import java.util.*;
 import java.io.*;
-public class IntegerReverse {
+public class Solution {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
@@ -8,8 +8,14 @@ public class IntegerReverse {
     }
 
     private static String reverseInteger(String s){
+        boolean positive = true;
+        if(s.charAt(0) == '-'){
+            positive = false;
+            s = s.substring(1);
+        }
         StringBuilder res = new StringBuilder(s);
-        return removeLeadingZeroes(res.reverse().toString());
+        String ans = removeLeadingZeroes(res.reverse().toString());
+        return positive || ans.equals("0") ? ans : "-" + ans; 
     }
 
     private static String removeLeadingZeroes(String s){
